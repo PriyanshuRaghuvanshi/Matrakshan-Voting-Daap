390357c58e2f3b6ec319523c2c74879a0aed44e8
PriyanshuRaghuvanshi <addresspriyanshuraghuvanshi18@gmail.com>
2022-07-22 02:53:54 +0530

    a

---
+++ block.js
@@ -0,0 +1,131 @@
+var web3;
+var address="0xf0C7488C259F5442096c030C1691a63489f30ec2";
+
+async function connect(){
+    await window.web3.currentProvider.enable();
+    web3=new Web3(window.web3.currentProvider);
+}
+
+if(typeof web3 !=='undefine'){
+    web3=new Web3(window.web3.currentProvider);
+}
+else{
+    web3=new Web3(new Web3.Provider.HttpProvider("HTTP://127.0.0.1:7545"));
+
+}
+
+var abi=[
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_person",
+				"type": "address"
+			}
+		],
+		"name": "authorize",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_address",
+				"type": "address"
+			}
+		],
+		"name": "candidate1F",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_address",
+				"type": "address"
+			}
+		],
+		"name": "candidate2F",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_address",
+				"type": "address"
+			}
+		],
+		"name": "candidate3F",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "string",
+				"name": "_name",
+				"type": "string"
+			}
+		],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [],
+		"name": "purpose",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "resultOfVoting",
+		"outputs": [
+			{
+				"internalType": "string",
+				"name": "",
+				"type": "string"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "totalVotesF",
+		"outputs": [
+			{
+				"internalType": "uint256",
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+
+var contract = new web3.eth.Contract(abi,address);
+
+let counterDisplayElem = document.queryselector('.')
+
